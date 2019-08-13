28af7e11a474c2d6fa36fbff142e6f218f0e31ba
David Mogbeyi <mogbeyidavid@gmail.com>
2019-08-13 16:33:35 +0100

    Added status code

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
     res.send({
-        message: "hello world"
+        message: "hello world",
+        status: 200
     });
 });
 
